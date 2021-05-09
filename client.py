e2565e3be5297a8e3d259370d0b553e47040824c
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2021-05-08 23:45:15 -0300

    Add small alterations

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket
 import threading
+import os
 
-nickname = input("Escolha um nome de usuário: ")
+print("\n Seja bem vindo ou bem vinda ao software de conversas da APS 2021!")
+input("\n\n Aperte qualquer coisa para continuar...")
+os.system('cls || clear')
+
+quit = print("[PARA SAIR, APERTE CONTROL E C AO MESMO TEMPO]")
+username = input("\nEscolha um nome de usuário: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 42))
@@ -10,8 +16,8 @@ def receive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            if message == 'NICK':
-                    client.send(nickname.encode('ascii'))
+            if message == 'USER':
+                    client.send(username.encode('ascii'))
             else:
                 print(message)
         except:
@@ -21,24 +27,25 @@ def receive():
 
 def write():
     while True:
-        a=input("")
-        if a == ":feliz:":
-            a = ":)"
-        elif a == ":triste:":
-            a = "):"
-        elif a == ":tedio:":
-            a = ":|"
-        elif a == ":raiva:":
-            a = ">:O"
-        elif a == ":surpresa:":
-            a = ":O"
-        elif a == ":rawr:":
-            a = "*-*"
-        elif a == ":lol:":
-            a = ":P"
+        text_message=input("Mande uma mensagem: ")
+        # --- emojis ---
+        if text_message == ":feliz:":
+            text_message = ":)"
+        elif text_message == ":triste:":
+            text_message = "):"
+        elif text_message == ":tedio:":
+            text_message = ":|"
+        elif text_message == ":raiva:":
+            text_message = ">:O"
+        elif text_message == ":surpresa:":
+            text_message = ":O"
+        elif text_message == ":rawr:":
+            text_message = "*-*"
+        elif text_message == ":lol:":
+            text_message = ":P"
         else:
             pass
-        message = f'{nickname}: {a}'
+        message = f'{username}: {text_message}'
         client.send(message.encode('ascii'))
 
 receive_thread = threading.Thread(target=receive)
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ server.bind((host, port))
 server.listen()
 
 clients = []
-nicknames = []
+usernames = []
 
 def broadcast(message):
     for client in clients:
@@ -23,9 +23,9 @@ def handle(client):
             index = clients.index(client)
             clients.remove(client)
             client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname} saiu do chat'.encode('ascii'))
-            nicknames.remove(nickname)
+            username = usernames[index]
+            broadcast(f'{username} saiu do chat'.encode('ascii'))
+            usernames.remove(username)
             break
 
 def receive():
@@ -33,19 +33,20 @@ def receive():
         client, address = server.accept()
         print(f'conectado com {str(address)}')
 
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode("ascii")
-        nicknames.append(nickname)
+        client.send('USER'.encode('ascii'))
+        username = client.recv(1024).decode("ascii")
+        usernames.append(username)
         clients.append(client)
 
-        print(f'O nome de usuário do cliente é {nickname}!')
-        broadcast(f'{nickname} Entrou no grupo! Seja bem vindo ao software de conversas da APS!'.encode('ascii'))
+        print(f'O nome de usuário do cliente é {username}!')
+        broadcast(f'{username} Entrou no grupo! Seja bem vindo ao software de conversas da APS!'.encode('ascii'))
         client.send('connectado ao servidor!'.encode('ascii'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-print("O servidor esta escutando...")
+print("O servidor esta escutando...\n")
+print("[Para fechar o servidor, por favor feche o terminal]\n")
 receive()
 
 
